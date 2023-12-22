47723c2bc3bf3498cd6b21839bc68be3976d0a46
abubakar471 <abdurjoy2001@gmail.com>
2023-12-22 22:07:31 +0600

    changed backend api url in frontend with the live backend url

--- frontend/src/components/single-chat/SingleChat.jsx
+++ frontend/src/components/single-chat/SingleChat.jsx
@@ -13,7 +13,7 @@ import Lottie from "react-lottie";
 import animationData from "../../animations/typing.json";
 
 
-const ENDPOINT = "http://localhost:8000";
+const ENDPOINT = "https://chat-module.onrender.com";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
