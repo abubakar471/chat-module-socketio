17105351ee5eea3d515bb223c2107162d39d59e9
abubakar471 <abdurjoy2001@gmail.com>
2024-03-16 22:06:22 +0600

    updated for final changes

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: [process.env.CLIENT_URL, process.env.IQSOCIAL_SERVER]
+    origin: [process.env.CLIENT_URL, process.env.IQSOCIAL_API_URL]
 }))
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
